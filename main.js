424a348be8460b3f1874fd8393bec3ecc0a52de8
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2022-04-28 11:38:35 -0500

    Added token reader from TOKEN.txt
    Reads TOKEN.txt and finds the value after TOKEN= and then finds the value within the double quotes.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function readToken() {
-    let token = "";
+var fs = require('fs');
 
-    // #TODO: MAKE READ TOKEN FUNCTION FROM "TOKEN.txt"
-    return token;
+try {  
+    var token = fs.readFileSync('TOKEN.txt', 'utf8');
+} catch(e) {
+    console.log('Error:', e.stack);
 }
+
+console.log(token.split("TOKEN=")[1].split('"')[1]);

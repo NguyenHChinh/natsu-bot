f8a8f9ec168d478a06382c91d175803a4c87f08e
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2023-01-25 11:34:07 -0600

    Added working pricecheck function
    !pricecheck command uses python-requests to receive information from Lost Ark Market and parse info for basic oreha fusion material

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
-
+import requests
 import discord
 from discord.ext import commands
 
 with open('config.json') as f:
     config = json.load(f)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,5 +15,19 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@bot.command()
+async def pricecheck(ctx):
+    url = "https://www.lostarkmarket.online/api/export-market-live/North America East"
+
+    parameters= {'items':'basic-oreha-fusion-material-2'}
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload, params=parameters)
+    data = response.json()
+    basic_oreha = data[0]
+
+    await ctx.send("Price of Oreha: " + str(round(basic_oreha["avgPrice"])))
 
 bot.run(config["token"])

4523832950250257360d32f86bb4d530b27e26d3
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2023-02-03 00:26:47 -0600

    Added week profit and rearranged argument check

--- main.py
+++ main.py
@@ -31,24 +31,25 @@ async def pricecheck(ctx, *args):
     if len(args) == 0:
         reduction.append(0)
         reduction.append(0)
-    elif (len(args) > 2):
-        await ctx.send("```Please check your arguments! Reason: Invalid Arguments\n\n" +
-                "Correct Command Usage: pricecheck [cost reduction] [time reduction]```")
-        return
-    else:
+    elif (len(args) < 3):
         # Creates reduction variable and verifies validity of each argument
         # Reduction varaible format will be [cost_reduction, time_reduction
-        for i in args: 
-            i = re.sub(r'[^(0-9) + .]', '', i) # Processing argument to only number value (remove %)
+        for i in args:
+            # Processing argument to only number value (remove %)
+            i = re.sub(r'[^(0-9) + .]', '', i)
 
-            if float(i) < 0 or float(i) > 100: # Invalid Response
+            if float(i) < 0 or float(i) > 100:  # Invalid Response
                 await ctx.send("```Please check your arguments! Reason: Invalid Arguments, Must Be Between 0-100```")
                 return
-            else: # Valid Response
+            else:  # Valid Response
                 reduction.append(float(i))
-        
+
         if len(reduction) == 1:
             reduction.append(0)
+    else:
+        await ctx.send("```Please check your arguments! Reason: Invalid Arguments\n\n" +
+                       "Correct Command Usage: pricecheck [cost reduction] [time reduction]```")
+        return
 
     # Using lostarkmarket.online api to receive information on each items
     url = "https://www.lostarkmarket.online/api/export-market-live/North America East"
@@ -82,30 +83,17 @@ async def pricecheck(ctx, *args):
 
     basic_crafting = {'materials': [oreha_solar_carp, natural_pearl, fish],
                       'amount': [10, 40, 80],
-                      'raw': base_basic}
+                      'raw': base_basic,
+                      'data': basic_oreha_fusion_material,
+                      'output': 30,
+                      'time': 45}
 
     superior_crafting = {'materials': [oreha_solar_carp, natural_pearl, fish],
                          'amount': [16, 64, 128],
-                         'raw': base_superior}
-
-    embed = Embed(title="Lost Ark Fusion Material Crafting", description="")
-    embed.color = 0x3498db
-    embed.set_thumbnail(
-        url="https://cdn.discordapp.com/emojis/967248060407250954.webp?size=240&quality=lossless")
-
-    if (show_average):
-        average_prices = "```\n"
-        average_prices += "Oreha Solar Carp (x10) - " + \
-            str(round(oreha_solar_carp['avgPrice'])) + 'g\n'
-        average_prices += "Natural Pearl (x10) - " + \
-            str(round(natural_pearl['avgPrice'])) + 'g\n'
-        average_prices += "Fish (x100) - " + str(round(fish['avgPrice'])) + 'g\n'
-        average_prices += "Basic Oreha Fusion Material (x1): - " + str(
-            round(basic_oreha_fusion_material['avgPrice'])) + 'g\n'
-        average_prices += "Superior Oreha Fusion Material (x1) - " + str(
-            round(superior_oreha_fusion_material['avgPrice'])) + 'g```\n\n'
-        embed.add_field(name="**Prices Used in Calculation**",
-                        value=average_prices, inline=False)
+                         'raw': base_superior,
+                         'data': superior_oreha_fusion_material,
+                         'output': 20,
+                         'time': 60}
 
     def singleUnit(item):
         return round(item['avgPrice']) / item['amount']
@@ -119,26 +107,68 @@ async def pricecheck(ctx, *args):
         temp_sum += round(recipe['raw'] * (1 - (float(reduction[0])/100)))
         return round(temp_sum, 2)
 
+    def calculateProfit(recipe):
+        # Return array of batch profit and unit profit
+        profit = []
+
+        # Calculate batch
+        batch = (round(recipe['data']['avgPrice']) - 1) * recipe['output']
+        batch -= calculateCost(recipe)
+        profit.append(round(batch, 2))
+
+        # Calculate unit
+        unit = round(recipe['data']['avgPrice']) - 1
+        unit -= calculateCost(recipe) / recipe['output']
+        profit.append(round(unit, 2))
+
+        return profit
+
+    def calculatePotential(recipe):
+        time_to_cost = recipe['time'] * (1 - (reduction[1] / 100))
+        batches_per_week = (10080 / time_to_cost) * 3
+        potential = batches_per_week * calculateProfit(recipe)[0]
+        return round(potential, 2)
+
+    embed = Embed(title="Lost Ark Fusion Material Crafting", description="")
+    embed.color = 0x3498db
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/emojis/967248060407250954.webp?size=240&quality=lossless")
+
+    if (show_average):
+        average_prices = "```\n"
+        average_prices += "Oreha Solar Carp (x10) - " + \
+            str(round(oreha_solar_carp['avgPrice'])) + 'g\n'
+        average_prices += "Natural Pearl (x10) - " + \
+            str(round(natural_pearl['avgPrice'])) + 'g\n'
+        average_prices += "Fish (x100) - " + \
+            str(round(fish['avgPrice'])) + 'g\n'
+        average_prices += "Basic Oreha Fusion Material (x1): - " + str(
+            round(basic_oreha_fusion_material['avgPrice'])) + 'g\n'
+        average_prices += "Superior Oreha Fusion Material (x1) - " + str(
+            round(superior_oreha_fusion_material['avgPrice'])) + 'g```\n\n'
+        embed.add_field(name="**Prices Used in Calculation**",
+                        value=average_prices, inline=False)
+
     if (show_basic):
-        crafting_operation_basic = "```\n"
-        crafting_operation_basic += "10 Oreha Solar Carp - " + \
+        craft_basic = "```\n"
+        craft_basic += "10 Oreha Solar Carp - " + \
             str(singleUnit(oreha_solar_carp) * 10) + 'g\n'
-        crafting_operation_basic += "40 Natural Pearl - " + \
+        craft_basic += "40 Natural Pearl - " + \
             str(singleUnit(natural_pearl) * 40) + 'g\n'
-        crafting_operation_basic += "80 Fish - " + \
+        craft_basic += "80 Fish - " + \
             str(round((singleUnit(fish) * 80), 2)) + 'g\n'
-        crafting_operation_basic += "Raw Gold - " + \
-            str(round(base_basic *
-                (100-(reduction[0]))/100)) + 'g (' + str(reduction[0]) + '% reduction)\n'
-        crafting_operation_basic += '\nSum Cost (Batch/Unit): ' + str(calculateCost(
+        craft_basic += "Raw Gold - " + str(round(base_basic *
+                                                 (100-(reduction[0]))/100)) + 'g (' + str(reduction[0]) + '% reduction)\n'
+        craft_basic += '\nSum Cost (Batch/Unit): ' + str(calculateCost(
             basic_crafting)) + 'g/' + str(round((calculateCost(basic_crafting) / 30), 2)) + 'g\n'
-        crafting_operation_basic += "Net Profit (Batch/Unit): "
-        crafting_operation_basic += str(round((((round(basic_oreha_fusion_material['avgPrice']) - 1) * 30) - calculateCost(basic_crafting)), 2))
-        crafting_operation_basic += "g/"
-        crafting_operation_basic += str(round(round(basic_oreha_fusion_material['avgPrice']) - ((calculateCost(basic_crafting) / 30) + 1), 2)) + 'g```'
+        craft_basic += "Profit (Batch/Unit): " + \
+            str(calculateProfit(basic_crafting)[0]) + "g/"
+        craft_basic += str(calculateProfit(basic_crafting)[1]) + 'g\n'
+        craft_basic += "\nPotential Profit Per Week: " + \
+            str(calculatePotential(basic_crafting)) + 'g```'
 
-        embed.add_field(name="**Basic Oreha Fusion Material Calculation**", value=crafting_operation_basic, inline=False)
-
+        embed.add_field(name="**Basic Oreha Fusion Material Calculation**",
+                        value=craft_basic, inline=False)
 
     if (show_superior):
         craft_superior = "```\n"
@@ -152,15 +182,16 @@ async def pricecheck(ctx, *args):
                                                     (100-(reduction[0]))/100)) + 'g (' + str(reduction[0]) + '% reduction)\n'
         craft_superior += '\nSum Cost (Batch/Unit): ' + str(calculateCost(
             superior_crafting)) + 'g/' + str(round((calculateCost(superior_crafting) / 20), 2)) + 'g\n'
-        craft_superior += "Profit (Batch/Unit): "
-        craft_superior += str(round((((round(superior_oreha_fusion_material['avgPrice']) - 1) * 20) - calculateCost(superior_crafting)), 2))
-        craft_superior += "g/"
-        craft_superior += str(round(round(superior_oreha_fusion_material['avgPrice']) - ((calculateCost(superior_crafting) / 20) + 1), 2)) + 'g```'
+        craft_superior += "Profit (Batch/Unit): " + \
+            str(calculateProfit(superior_crafting)[0]) + "g/"
+        craft_superior += str(calculateProfit(superior_crafting)[1]) + 'g\n'
+        craft_superior += "\nPotential Profit Per Week: " + \
+            str(calculatePotential(superior_crafting)) + 'g```'
 
-        embed.add_field(name="**Superior Oreha Fusion Material Calculation**", value=craft_superior, inline=False)
+        embed.add_field(name="**Superior Oreha Fusion Material Calculation**",
+                        value=craft_superior, inline=False)
 
     # Send the embed to the channel
-    print(ctx)
     await ctx.send(embed=embed)
 
 

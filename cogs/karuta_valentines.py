1c35b3e83502eb54967e2ba65ea100b57eb0fe55
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2023-02-20 12:21:19 -0600

    Cleaned up file
    Moved variables outside of function so that it is easier to change up if I ever needed to use this as a template for another event

--- cogs/karuta_valentines.py
+++ cogs/karuta_valentines.py
@@ -3,11 +3,21 @@ from discord.ext import commands
 
 # Adjustable variables
 print_buttons = False
+target = ['🌻', '🌹', '🌼', '🌷']
+sunflower_role = 1073733358334513152
+rose_role = 1073733260573671506
+blossom_role = 1073733365129297960
+tulip_role = 1073733365867483196
+role_ids = [sunflower_role, rose_role, blossom_role, tulip_role]
+
 
 class karuta_valentines(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        if ("aki " in message.content): 
+        if ("aki " in message.content):
             await commands.bot.process_commands(message)
 
         if (message.author.id != 646937666251915264):
@@ -19,23 +29,18 @@ class karuta_valentines(commands.Cog):
         channel = discord.utils.get(
             message.guild.channels, name="karuta-fiends")  # our channel
 
-        flowers = ['🌻', '🌹', '🌼', '🌷']
-        role_ids = [1073733358334513152, 1073733260573671506,
-                    1073733365129297960, 1073733365867483196]
-
         for button in message.components:
             emojis = []
             for child in button.children:
                 emoji = child.emoji.name
                 emojis.append(emoji)
-                if emoji in flowers:
+                if emoji in target:
                     print(f'FLOWER: {emoji}')
-                    flower_index = flowers.index(emoji)
+                    flower_index = target.index(emoji)
                     await message.channel.send(f'Hey! A <@&{role_ids[flower_index]}> dropped!')
             if (print_buttons):
                 print(f'Card drop detected! Buttons: {emojis}')
-            else:
-                print(f'Print_buttons is off')
+
 
 async def setup(bot):
     await bot.add_cog(karuta_valentines(bot))

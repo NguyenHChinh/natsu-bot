d3ec111bd62d3c53107472faf9d8af5738d898d9
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2023-03-05 00:27:47 -0600

    Cleaned up code

--- cogs/karuta_drop.py
+++ cogs/karuta_drop.py
@@ -31,38 +31,38 @@ class karuta_drop(commands.Cog):
         channel = discord.utils.get(
             message.guild.channels, name="karuta-fiends")  # our channel
 
-        path = os.path.join(os.getcwd(), 'cogs', 'temp')
-        if not os.path.exists(path):
-            os.makedirs(path)
-        file_path = os.path.join(path, 'karuta_drop.jpg')
+        output_dir = os.path.join(os.getcwd(), 'cogs', 'temp')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        karuta_drop_path = os.path.join(output_dir, 'karuta_drop.jpg')
         img_data = requests.get(message.attachments[0].url).content
-        with open(file_path, 'wb') as handler:
+        with open(karuta_drop_path, 'wb') as handler:
             handler.write(img_data)
 
         time.sleep(2)
 
         pytesseract.tesseract_cmd = path_to_tesseract
-        img = Image.open(file_path)
+        img = Image.open(karuta_drop_path)
 
         cards = []
         top = 67
         bottom = 105
         card1 = img.crop((50, top, 230, bottom))
         cards.append(pytesseract.image_to_string(card1))
-        card1.convert('RGB').save(os.path.join(file_path, 'card1.jpg'))
+        card1.convert('RGB').save(os.path.join(output_dir, 'card1.jpg'))
 
         card2 = img.crop((320, top, 505, bottom))
         cards.append(pytesseract.image_to_string(card2))
-        card2.convert('RGB').save(os.path.join(file_path, 'card2.jpg'))
+        card2.convert('RGB').save(os.path.join(output_dir, 'card2.jpg'))
 
         card3 = img.crop((600, top, 780, bottom))
         cards.append(pytesseract.image_to_string(card3))
-        card3.convert('RGB').save(os.path.join(file_path, 'card3.jpg'))
+        card3.convert('RGB').save(os.path.join(output_dir, 'card3.jpg'))
 
         if (img.width > 840):
             card4 = img.crop((870, top, 1050, bottom))
             cards.append(pytesseract.image_to_string(card4))
-            card4.convert('RGB').save(os.path.join(file_path, 'card4.jpg'))
+            card4.convert('RGB').save(os.path.join(output_dir, 'card4.jpg'))
 
         print_message = ''
         for i in cards:

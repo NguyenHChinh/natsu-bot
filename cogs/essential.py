b8ec07fcdf4ec5f6835b64dca414b0a1ef056df8
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2024-03-15 11:26:29 -0500

    Adding config functionality and removal of personal data

--- cogs/essential.py
+++ cogs/essential.py
@@ -1,9 +1,14 @@
 # essential.py
-# This file serves to host very basic commands for personal testing of bot
+# This file serves to host very basic commands for testing of bot as well as a template for new cogs
 
+# Importing required libraries
 import discord
+import json
 from discord.ext import commands
 
+# Load the client.json file
+with open('./config.json') as f:
+    config = json.load(f)
 
 class essential(commands.Cog):
     def __init__(self, bot):
@@ -15,12 +20,12 @@ class essential(commands.Cog):
         await ctx.send("Pong")
 
     @commands.command()
-    async def test(self, ctx):
-        # Checks to see if user who called this is me (Aki#1005)
-        if (ctx.author.id != 202872300968607745):
+    async def amiadmin(self, ctx):
+        # Checks to see if user who called 
+        if (ctx.author.id != config['discord_admin_id']):
             return
 
-        await ctx.send("Test!")
+        await ctx.send("Yes, you are set as admin.")
 
 
 async def setup(bot):
